c9b59ff70291d961b64cdb53d784629cc2a8aa9e
Jakob Dam Jensen <jakob@nopointlessapps.com>
2020-09-28 13:07:53 +0200

    Add ability to pass options to subscriptions apis (#3)

--- lib/subscriptions.ex
+++ lib/subscriptions.ex
@@ -4,28 +4,32 @@ defmodule Quickpay.Subscriptions do
 
   import Quickpay.Client
 
-  def create(%Client{} = client, order_id, currency, description)
+  def create(%Client{} = client, order_id, currency, description, options \\ %{})
       when is_binary(order_id) and is_binary(currency) and is_binary(description) do
     post!(
       client.host <> "subscriptions",
       [authorization_header(client)],
-      to_json!(%{
-        "order_id" => order_id,
-        "currency" => currency,
-        "description" => description
-      })
+      to_json!(
+        Map.merge(options, %{
+          "order_id" => order_id,
+          "currency" => currency,
+          "description" => description
+        })
+      )
     )
     |> Quickpay.StructUtils.convert_http_response_to_struct(201, Subscription)
   end
 
-  def link(%Client{} = client, subscription_id, amount) do
+  def link(%Client{} = client, subscription_id, amount, options \\ %{}) do
     put!(
       "#{client.host}subscriptions/#{subscription_id}/link",
       [authorization_header(client)],
-      to_json!(%{
-        "amount" => amount,
-        "payment_methods" => "creditcard"
-      })
+      to_json!(
+        Map.merge(options, %{
+          "amount" => amount,
+          "payment_methods" => "creditcard"
+        })
+      )
     )
     |> Quickpay.StructUtils.convert_http_response_to_struct(200, Quickpay.Link)
   end
@@ -38,14 +42,22 @@ defmodule Quickpay.Subscriptions do
     |> Quickpay.StructUtils.convert_http_response_to_struct(200, Subscription)
   end
 
-  def recurring(%Client{} = client, %Subscription{id: subscription_id}, order_id, amount) do
+  def recurring(
+        %Client{} = client,
+        %Subscription{id: subscription_id},
+        order_id,
+        amount,
+        options \\ %{}
+      ) do
     post!(
       client.host <> "subscriptions/#{subscription_id}/recurring",
       [authorization_header(client)],
-      to_json!(%{
-        "order_id" => order_id,
-        "amount" => amount
-      })
+      to_json!(
+        Map.merge(options, %{
+          "order_id" => order_id,
+          "amount" => amount
+        })
+      )
     )
     |> Quickpay.StructUtils.convert_http_response_to_struct(202, Quickpay.Payment)
   end
